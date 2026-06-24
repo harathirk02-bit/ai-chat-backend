318b2e286f4829ec8ba9fb06ed1691db33fcb44a
Harathi <harathirk02@gmail.com>
2026-06-24 18:41:24 +0530

    Update Gemini API key and model version

--- services/chatbot_service.py
+++ services/chatbot_service.py
@@ -3,11 +3,11 @@ import os
 
 # Gemini API Setup
 genai.configure(
-    api_key=os.getenv("GEMINI_API_KEY")
+    api_key=os.getenv("AQ.Ab8RN6KXe7_BlqinMpmkA5xIUci_ZtrRNqtC9Zvx9GIfNU2n-Q")
 )
 
 # Load model
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 def chatbot_reply(question: str):
